8dc5466baf2cd701bc2845c5c2193df2ee60d821
Charles Daniels <cdaniels@fastmail.com>
2015-08-30 12:02:51 -0400

    added support for matlab .mat format, still testing though

--- libSparseConvert.py
+++ libSparseConvert.py
@@ -463,12 +463,70 @@ class sparseConvert:
 				
 				this.HSM.nzentries = len(this.HSM.elements['val'])
 				
+			
+
+
 					
 			except IOError as e: # make sure the file exists and is readable
 				this.logger.log("could not open matrix file (375)", "error")
 				raise IOError("could not open matrix file for writing...",
 							  str(e))
 			
+
+		elif form == 'mat': # matlab matrices 
+			from scipy import io
+			from scipy import sparse 
+			from numpy import array 
+
+			try:
+				# TODO: this code should be modularized, as it is basically
+				# identical to the mtx code, other than loadmat vs mmread 
+				
+				rawMatrix = scipy.sparse.coo_matrix(scipy.io.loadmat(filename)) 
+	
+				if 'symmetric' in io.mminfo(filename):
+					this.HSM.symmetry = "SYM"
+				else:
+					this.HSM.symmetry = "ASYM"
+
+				hercm = {} # needed to generate verification 
+	
+				hercm['val'] 			= rawMatrix.data
+				hercm['col'] 			= rawMatrix.col.tolist()
+				hercm['row'] 			= rawMatrix.row.tolist()
+				(matrixWidth, matrixHeight) = rawMatrix.shape
+				this.HSM.height			= int(matrixHeight)
+				this.HSM.width 			= int(matrixWidth)
+				vs = this.HERCMIO.generateVerificationSum(hercm)
+				this.HSM.verification   = vs 
+				this.HSM.remarks		= []
+
+
+				# I'm not sure why this has to be hard...
+				# http://stackoverflow.com/questions/26018781/numpy-is-it-possible-to-preserve-the-dtype-of-columns-when-using-column-stack
+
+				val = numpy.asarray(hercm['val'], dtype='float64')
+				col = numpy.asarray(hercm['col'], dtype='int32')
+				row = numpy.asarray(hercm['row'], dtype='int32')
+
+				val = numpy.rec.array(val, dtype=[(('val'), numpy.float64)])
+				col = numpy.rec.array(col, dtype=[(('col'), numpy.int32)])
+				row = numpy.rec.array(row, dtype=[(('row'), numpy.int32)])
+
+				this.HSM.elements = append_fields(row, 'col', col, usemask = False, dtypes=[numpy.int32])
+				this.HSM.elements = append_fields(this.HSM.elements, 'val', val, usemask = False, dtypes=[numpy.float64])
+
+				
+				this.HSM.nzentries = len(this.HSM.elements['val'])
+				
+			
+		
+			except IOError as e: # make sure the file exists and is readable
+				this.logger.log("could not open matrix file (375)", "error")
+				raise IOError("could not open matrix file for writing...",
+							  str(e))
+
+
 
 		else:
 			this.logger.log("format must be hercm or mtx, cannot use format {0}"
@@ -533,6 +591,15 @@ class sparseConvert:
 				with open(filename,'w') as outputFile:
 					for line in lines:
 						outputFile.write(line) 
+
+		elif form == 'mat': # matlab matrix file 
+			from scipy import io
+			from scipy import sparse 
+			from numpy import array 
+
+			scipy.io.savemat(filename, 
+				scipy.sparse.m_dict(this.HSM.getInFormat('coo')))
+
 
 		else:
 			this.logger.log("format must be hercm or mtx, cannot use format {0}"
