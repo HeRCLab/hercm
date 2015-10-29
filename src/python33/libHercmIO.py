91f0fda1216cbf160b33491ad1d5000c4c5c4310
Charles Daniels <cdaniels@fastmail.com>
2015-10-29 19:31:33 -0400

    got rid of uneeded classes... now to start seeing what breaks

--- src/python33/libHercmIO.py
+++ src/python33/libHercmIO.py
@@ -16,250 +16,242 @@ import logging
 """
 
 
+def readMatrix(this, filename, form, showProgress=False):
+    # filename is the string path to the matrix file to open
+    # format is the string 'hercm' or 'mtx' specifying matrix format
 
-class hercmIO:
+    # reads the matrix into this.HSM for later processing
+    # converts non-hercm matrices to hercm internally
 
-    def __init__(this):
-        # TODO: change this name from HSM to something
-        this.HSM = libHercMatrix.hercMatrix()
-        # more relevant
-        this.HERCMIO = libBXF.bxfio()
+    logging.info("reading matrix {0} in format {1}".format(filename, form))
 
-    def readMatrix(this, filename, form, showProgress = False):
-        # filename is the string path to the matrix file to open
-        # format is the string 'hercm' or 'mtx' specifying matrix format
+    if form == ('hercm' or 'bxf'):
+        if showProgress:
+            print("format is bxf, reading matrix using HERCMIO...")
+        matrix = None
+        try:
+            matrix = this.HERCMIO.read(filename)
+        except Exception as e:
+            print("ERROR: could not read matrix")
+            print("stack trace...")
+            print(traceback.format_exc())
 
-        # reads the matrix into this.HSM for later processing
-        # converts non-hercm matrices to hercm internally
+        this.HSM = matrix
+        this.HSM.nzentries = len(this.HSM.elements['val'])
 
-        logging.info("reading matrix {0} in format {1}".format(filename, form))
+        if showProgress:
+            print("finished reading matrix.")
 
-        if form == ('hercm' or 'bxf'):
+    elif form == 'mtx':
+        from scipy import io
+        from scipy.sparse import csr_matrix
+        from numpy import array
+
+        # reads in an MTX file and converts it to hercm
+
+        try:
             if showProgress:
-                print("format is bxf, reading matrix using HERCMIO...")
-            matrix = None
-            try:
-                matrix = this.HERCMIO.read(filename)
-            except Exception as e:
-                print("ERROR: could not read matrix")
-                print("stack trace...")
-                print(traceback.format_exc())
+                print("reading data from file...")
 
-            this.HSM = matrix
+            rawMatrix = scipy.sparse.coo_matrix(scipy.io.mmread(filename))
+
+            if 'symmetric' in io.mminfo(filename):
+                this.HSM.symmetry = "SYM"
+            else:
+                this.HSM.symmetry = "ASYM"
+
+            hercm = {}  # needed to generate verification
+
+            if showProgress:
+                print("generating header data..")
+            hercm['val'] = rawMatrix.data
+            hercm['col'] = rawMatrix.col.tolist()
+            hercm['row'] = rawMatrix.row.tolist()
+            (matrixWidth, matrixHeight) = rawMatrix.shape
+            this.HSM.height = int(matrixHeight)
+            this.HSM.width = int(matrixWidth)
+            vs = this.HERCMIO.generateVerificationSum(hercm)
+            this.HSM.verification = vs
+            this.HSM.remarks = []
+
+            # I'm not sure why this has to be hard...
+            # http://stackoverflow.com/questions/26018781/numpy-is-it-possible-to-preserve-the-dtype-of-columns-when-using-column-stack
+
+            if showProgress:
+                print("preparing matrix data...")
+            val = numpy.asarray(hercm['val'], dtype='float64')
+            col = numpy.asarray(hercm['col'], dtype='int32')
+            row = numpy.asarray(hercm['row'], dtype='int32')
+
+            val = numpy.rec.array(val, dtype=[(('val'), numpy.float64)])
+            col = numpy.rec.array(col, dtype=[(('col'), numpy.int32)])
+            row = numpy.rec.array(row, dtype=[(('row'), numpy.int32)])
+
+            this.HSM.elements = append_fields(row,
+                    'col',
+                    col,
+                    usemask=False,
+                    dtypes=[numpy.int32])
+
+            this.HSM.elements = append_fields(this.HSM.elements,
+                    'val',
+                    val,
+                    usemask=False,
+                    dtypes=[numpy.float64])
+
             this.HSM.nzentries = len(this.HSM.elements['val'])
 
             if showProgress:
-                print("finished reading matrix.")
+                print("finished reading matrix")
 
-        elif form == 'mtx':
-            from scipy import io
-            from scipy.sparse import csr_matrix
-            from numpy import array
+        except IOError as e:  # make sure the file exists and is readable
+            logging.warning("(lsc-480) could not open matrix file")
+            raise IOError("could not open matrix file for writing...",
+                          str(e))
 
-            # reads in an MTX file and converts it to hercm
+    elif form == 'mat':  # matlab matrices
+        from scipy import io
+        from scipy import sparse
+        from numpy import array
 
-            try:
-                if showProgress:
-                    print("reading data from file...")
-                
-                rawMatrix = scipy.sparse.coo_matrix(scipy.io.mmread(filename))
-                
-                if 'symmetric' in io.mminfo(filename):
-                    this.HSM.symmetry = "SYM"
-                else:
-                    this.HSM.symmetry = "ASYM"
+        try:
 
-                hercm = {}  # needed to generate verification
+            rawMatrix = scipy.sparse.coo_matrix(
+                scipy.io.loadmat(filename)['matrix'])
 
-                if showProgress:
-                    print("generating header data..")
-                hercm['val'] = rawMatrix.data
-                hercm['col'] = rawMatrix.col.tolist()
-                hercm['row'] = rawMatrix.row.tolist()
-                (matrixWidth, matrixHeight) = rawMatrix.shape
-                this.HSM.height = int(matrixHeight)
-                this.HSM.width = int(matrixWidth)
-                vs = this.HERCMIO.generateVerificationSum(hercm)
-                this.HSM.verification = vs
-                this.HSM.remarks = []
+            hercm = {}  # needed to generate verification
 
-                # I'm not sure why this has to be hard...
-                # http://stackoverflow.com/questions/26018781/numpy-is-it-possible-to-preserve-the-dtype-of-columns-when-using-column-stack
+            hercm['val'] = rawMatrix.data
+            hercm['col'] = rawMatrix.col.tolist()
+            hercm['row'] = rawMatrix.row.tolist()
+            (matrixWidth, matrixHeight) = rawMatrix.shape
+            this.HSM.height = int(matrixHeight)
+            this.HSM.width = int(matrixWidth)
+            vs = this.HERCMIO.generateVerificationSum(hercm)
+            this.HSM.verification = vs
+            this.HSM.remarks = []
 
-                if showProgress:
-                    print("preparing matrix data...")
-                val = numpy.asarray(hercm['val'], dtype='float64')
-                col = numpy.asarray(hercm['col'], dtype='int32')
-                row = numpy.asarray(hercm['row'], dtype='int32')
+            # I'm not sure why this has to be hard...
+            # http://stackoverflow.com/questions/26018781/numpy-is-it-possible-to-preserve-the-dtype-of-columns-when-using-column-stack
 
-                val = numpy.rec.array(val, dtype=[(('val'), numpy.float64)])
-                col = numpy.rec.array(col, dtype=[(('col'), numpy.int32)])
-                row = numpy.rec.array(row, dtype=[(('row'), numpy.int32)])
+            val = numpy.asarray(hercm['val'], dtype='float64')
+            col = numpy.asarray(hercm['col'], dtype='int32')
+            row = numpy.asarray(hercm['row'], dtype='int32')
 
-                this.HSM.elements = append_fields(row,
-                        'col',
-                        col,
-                        usemask=False,
-                        dtypes=[numpy.int32])
+            val = numpy.rec.array(val, dtype=[(('val'), numpy.float64)])
+            col = numpy.rec.array(col, dtype=[(('col'), numpy.int32)])
+            row = numpy.rec.array(row, dtype=[(('row'), numpy.int32)])
 
-                this.HSM.elements = append_fields(this.HSM.elements,
-                        'val',
-                        val,
-                        usemask=False,
-                        dtypes=[numpy.float64])
+            this.HSM.elements = append_fields(row,
+                    'col',
+                    col,
+                    usemask=False,
+                    dtypes=[numpy.int32])
 
-                this.HSM.nzentries = len(this.HSM.elements['val'])
+            this.HSM.elements = append_fields(this.HSM.elements,
+                    'val', val, usemask=False, dtypes=[numpy.float64])
 
-                if showProgress:
-                    print("finished reading matrix")
+            this.HSM.nzentries = len(this.HSM.elements['val'])
 
-            except IOError as e:  # make sure the file exists and is readable
-                logging.warning("(lsc-480) could not open matrix file")
-                raise IOError("could not open matrix file for writing...",
-                              str(e))
+            if this.HSM.checkSymmetry():
+                this.HSM.symmetry = 'SYM'
 
-        elif form == 'mat':  # matlab matrices
-            from scipy import io
-            from scipy import sparse
-            from numpy import array
+        except IOError as e:  # make sure the file exists and is readable
+            logging.warning("(lsc-536)could not open matrix file")
+            raise IOError("could not open matrix file for writing...",
+                          str(e))
 
-            try:
+    else:
+        this.logger.log("format must be hercm or mtx, cannot use format {0}"
+                        .format(form), "error")
+        logging.warning("(lsc-545) format {0} is not valid".format(form))
 
-                rawMatrix = scipy.sparse.coo_matrix(
-                    scipy.io.loadmat(filename)['matrix'])
+    if showProgress:
+        print("converting matrix to row-major...")
+    logging.info("converting matrix to row-major")
+    this.HSM.makeRowMajor()
 
-                hercm = {}  # needed to generate verification
+    if showProgress:
+        print("matrix is now row major")
 
-                hercm['val'] = rawMatrix.data
-                hercm['col'] = rawMatrix.col.tolist()
-                hercm['row'] = rawMatrix.row.tolist()
-                (matrixWidth, matrixHeight) = rawMatrix.shape
-                this.HSM.height = int(matrixHeight)
-                this.HSM.width = int(matrixWidth)
-                vs = this.HERCMIO.generateVerificationSum(hercm)
-                this.HSM.verification = vs
-                this.HSM.remarks = []
-
-                # I'm not sure why this has to be hard...
-                # http://stackoverflow.com/questions/26018781/numpy-is-it-possible-to-preserve-the-dtype-of-columns-when-using-column-stack
-
-                val = numpy.asarray(hercm['val'], dtype='float64')
-                col = numpy.asarray(hercm['col'], dtype='int32')
-                row = numpy.asarray(hercm['row'], dtype='int32')
-
-                val = numpy.rec.array(val, dtype=[(('val'), numpy.float64)])
-                col = numpy.rec.array(col, dtype=[(('col'), numpy.int32)])
-                row = numpy.rec.array(row, dtype=[(('row'), numpy.int32)])
-
-                this.HSM.elements = append_fields(row,
-                        'col',
-                        col,
-                        usemask=False,
-                        dtypes=[numpy.int32])
-
-                this.HSM.elements = append_fields(this.HSM.elements,
-                        'val', val, usemask=False, dtypes=[numpy.float64])
-
-                this.HSM.nzentries = len(this.HSM.elements['val'])
-
-                if this.HSM.checkSymmetry():
-                    this.HSM.symmetry = 'SYM'
-
-            except IOError as e:  # make sure the file exists and is readable
-                logging.warning("(lsc-536)could not open matrix file")
-                raise IOError("could not open matrix file for writing...",
-                              str(e))
-
-        else:
-            this.logger.log("format must be hercm or mtx, cannot use format {0}"
-                            .format(form), "error")
-            logging.warning("(lsc-545) format {0} is not valid".format(form))
-
+    if this.HSM.symmetry == 'SYM':
+        logging.info("matrix is symmetric, truncating lower triangle")
         if showProgress:
-            print("converting matrix to row-major...")
-        logging.info("converting matrix to row-major")
-        this.HSM.makeRowMajor()
-
+            print("matrix is symmetric, truncating lower triangle...")
+        this.HSM.makeSymmetrical('truncate', showProgress)
         if showProgress:
-            print("matrix is now row major")
+            print("lower triangle truncated")
 
-        if this.HSM.symmetry == 'SYM':
-            logging.info("matrix is symmetric, truncating lower triangle")
-            if showProgress:
-                print("matrix is symmetric, truncating lower triangle...")
-            this.HSM.makeSymmetrical('truncate', showProgress)
-            if showProgress:
-                print("lower triangle truncated")
 
-    def writeMatrix(this, filename, form):
-        # writes this.HSM to the file
-        # filename is a string indicating path of file
-        # format is a string indicating file format (mtx or hercm)
-        # returns True on success, None on failure
+def writeMatrix(this, filename, form):
+    # writes this.HSM to the file
+    # filename is a string indicating path of file
+    # format is a string indicating file format (mtx or hercm)
+    # returns True on success, None on failure
 
-        logging.info("writing matrix {0} in format {1}..."
-                .format(filename, form))
+    logging.info("writing matrix {0} in format {1}..."
+            .format(filename, form))
 
-        if os.path.isfile(filename):
-            logging.warning("(lsc-566) file exists, cannot write")
-            raise FileExistsError("could not write to file {0}".format(filename)
-                    + " file already exists!")
+    if os.path.isfile(filename):
+        logging.warning("(lsc-566) file exists, cannot write")
+        raise FileExistsError("could not write to file {0}".format(filename)
+                + " file already exists!")
 
-        if this.HSM.symmetry == 'SYM':
-            logging.info("matrix is symmetric, truncating lower triangle")
-            this.HSM.makeSymmetrical('truncate')
+    if this.HSM.symmetry == 'SYM':
+        logging.info("matrix is symmetric, truncating lower triangle")
+        this.HSM.makeSymmetrical('truncate')
 
-        logging.info("making matrix row major...")
-        this.HSM.makeRowMajor()
+    logging.info("making matrix row major...")
+    this.HSM.makeRowMajor()
 
-        if form == 'hercm':
-            try:
-                this.HERCMIO.write(this.HSM, filename, "HERCM")
-            except HercmioValidationError:
-                print("ERROR: matrix did not pass validation!")
-                print("Matrix will not be written to file")
+    if form == 'hercm':
+        try:
+            this.HERCMIO.write(this.HSM, filename, "HERCM")
+        except HercmioValidationError:
+            print("ERROR: matrix did not pass validation!")
+            print("Matrix will not be written to file")
 
-        if form == 'bxf':
-            try:
-                this.HERCMIO.write(this.HSM, filename)
-            except HercmioValidationError:
-                print("ERROR: matrix did not pass validation!")
-                print("Matrix will not be written to file")
+    if form == 'bxf':
+        try:
+            this.HERCMIO.write(this.HSM, filename)
+        except HercmioValidationError:
+            print("ERROR: matrix did not pass validation!")
+            print("Matrix will not be written to file")
 
-        elif form == 'mtx':
-            try:
-                scipy.io.mmwrite(filename, this.HSM.getInFormat('coo'))
-            except ValueError as e:
-                logging.warning("""(lsc-589) encountered ValueError exception 
+    elif form == 'mtx':
+        try:
+            scipy.io.mmwrite(filename, this.HSM.getInFormat('coo'))
+        except ValueError as e:
+            logging.warning("""(lsc-589) encountered ValueError exception 
 while writing file. Exception: {0}. You probably have out of bounds indices 
 in row or col""".format(e))
             except Exception as e:
                 logging.warning("""(lsc-593) encountered general error while
  writing: {0}""".format(str(e)))
 
-                raise IOError("encountered exception while writing matrix")
+            raise IOError("encountered exception while writing matrix")
 
-            # fix header of mtx file
-            if this.HSM.symmetry == 'SYM':
-                logging.info("format is mtx and matrix is symmetric, fixing" +
-                        " header...")
-                with open(filename) as inputFile:
-                    lines = inputFile.readlines()
-                lines[
-                    0] = "%%MatrixMarket matrix coordinate pattern symmetric\n"
+        # fix header of mtx file
+        if this.HSM.symmetry == 'SYM':
+            logging.info("format is mtx and matrix is symmetric, fixing" +
+                    " header...")
+            with open(filename) as inputFile:
+                lines = inputFile.readlines()
+            lines[
+                0] = "%%MatrixMarket matrix coordinate pattern symmetric\n"
 
-                with open(filename, 'w') as outputFile:
-                    for line in lines:
-                        outputFile.write(line)
+            with open(filename, 'w') as outputFile:
+                for line in lines:
+                    outputFile.write(line)
 
-        elif form == 'mat':  # matlab matrix file
-            from scipy import io
-            from scipy import sparse
-            from numpy import array
+    elif form == 'mat':  # matlab matrix file
+        from scipy import io
+        from scipy import sparse
+        from numpy import array
 
-            scipy.io.savemat(filename, {'matrix': this.HSM.getInFormat('coo')})
+        scipy.io.savemat(filename, {'matrix': this.HSM.getInFormat('coo')})
 
-        else:
-            logging.warning("(lsc-621) format {0} is not valid".format(form))
-            raise TypeError("{0} is not a valid format"
-                                           .format(form))
+    else:
+        logging.warning("(lsc-621) format {0} is not valid".format(form))
+        raise TypeError("{0} is not a valid format"
+                        .format(form))
