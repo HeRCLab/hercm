fd72676337b435ded6205ef731a9d5bb1e1a4e56
Charles A Daniels <cdaniels@fastmail.com>
2016-01-11 14:10:36 -0500

    added mallocs

--- src/c/sample.c
+++ src/c/sample.c
@@ -2,11 +2,16 @@
 
 int main(int argc, char **argv )
 {
-    char * version;
-    char * symmetry;
-    int * height;
-    int * width;
-    int * nnz;
-    bxfio_read_header("test", version, height, width, nnz, symmetry);
+    printf("Allocating memory for header data... ");
+    char * filename = (char*) malloc(512);
+    strcpy(filename, "test.bxf");
+    char * version = (char*) malloc(6);
+    char * symmetry = (char*) malloc(4);
+    int * height = (int*) malloc(1);
+    int * width = (int*) malloc(1);
+    int * nnz = (int*) malloc(1);
+    printf("done.\n");
+
+    bxfio_read_header(filename, version, height, width, nnz, symmetry);
 	return 0;
 }
